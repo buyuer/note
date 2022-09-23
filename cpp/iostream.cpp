ecbece66bc596bbeaf0f27577c32ea262a6445ea
buyuer <dingddding@163.com>
2022-09-23 15:56:15 +0800

    Add iostream.
    Signed-off-by: buyuer <dingddding@163.com>

---
+++ cpp/iostream.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> nums;
+
+    for (int num{}; std::cin >> num;)
+    {
+        nums.emplace_back(num);
+        if (std::cin.get() == '\n')
+            break;
+    }
+
+    for (auto num : nums)
+    {
+        std::cout << num << std::endl;
+    }
+}
