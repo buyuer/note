23d77f38211997d9ee64e2725abe63fd12627213
Junbo Zheng <zhengjunbo1@xiaomi.com>
2022-12-21 17:24:24 +0800

    unix/pthread.c: add mock data to compare with each other
    Signed-off-by: Junbo Zheng <zhengjunbo1@xiaomi.com>

--- unix/pthread.c
+++ unix/pthread.c
@@ -5,16 +5,20 @@
 
 static void *thread_entry(void *arg)
 {
-    printf("%p\n", arg);
+    printf("data addr: %p(after)\n", arg);
 
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    static unsigned char data = 0xaa;
+    printf("data addr: %p(before)\n", &data);
     pthread_t thread;
-    pthread_create(&thread, NULL, thread_entry, argv);
+    pthread_create(&thread, NULL, thread_entry, &data);
     pthread_detach(thread);
 
     sleep(1);
+
+    return 0;
 }
