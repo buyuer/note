e52ad060a765a3fb8e3b999e22eeecea2df0bef8
buyuer <dingddding@163.com>
2023-04-05 23:21:19 +0800

    添加android
    Signed-off-by: buyuer <dingddding@163.com>

---
+++ android/note.cpp
@@ -0,0 +1,13 @@
+#include "note.hpp"
+
+#include <android/api-level.h>
+
+namespace note
+{
+
+std::string demo()
+{
+    return std::to_string(android_get_device_api_level());
+}
+
+} // namespace note
