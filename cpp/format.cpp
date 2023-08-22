e4ec74855a1d4b759123ea3ec480cdaa252013f4
buyuer <dingddding@163.com>
2023-08-22 23:00:49 +0800

    Add format.
    Signed-off-by: buyuer <dingddding@163.com>

---
+++ cpp/format.cpp
@@ -0,0 +1,16 @@
+#include <format>
+#include <iostream>
+#include <iterator>
+#include <concepts>
+
+int main(int argc, char *argv[])
+{
+    std::cout << std::format("{}", 123) << std::endl;
+    std::cout << std::format("{:6}", 134) << std::endl;
+    std::cout << std::format("{:<2}", 100) << std::endl;
+
+    std::string str;
+    std::format_to(std::back_inserter( str), "{}", 10);
+    std::cout << str << std::endl;
+
+}
