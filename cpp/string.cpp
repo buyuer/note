d0ef9f3f55382e4b97cbd647284b629276f44339
buyuer <dingddding@163.com>
2024-01-19 00:05:33 +0800

    Add string
    Signed-off-by: buyuer <dingddding@163.com>

---
+++ cpp/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main(int argc, char *argv[])
+{
+    std::string str{"你好"};
+    std::cout << str.find("好")  << std::endl;
+
+    std::cout << str << std::endl;
+}
