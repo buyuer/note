f5dd38ee597a0e924ec44a5d61ed977ee0592b56
buyuer <dingddding@163.com>
2024-02-05 23:28:48 +0800

    添加qt test
    Signed-off-by: buyuer <dingddding@163.com>

---
+++ qt/test.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    auto             args = QCoreApplication::arguments();
+    for (auto &arg : args)
+    {
+        qInfo() << arg;
+    }
+}
