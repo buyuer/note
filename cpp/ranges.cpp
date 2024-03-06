cce58ddedd65cfe210f2f9fc6f4e1fb1d44611e6
buyuer <dingddding@163.com>
2024-03-06 22:28:38 +0800

    Add ranges for split
    Signed-off-by: buyuer <dingddding@163.com>

--- cpp/ranges.cpp
+++ cpp/ranges.cpp
@@ -1,18 +1,30 @@
+#include <iomanip>
 #include <iostream>
 #include <ranges>
-#include <vector>
+#include <string>
+#include <string_view>
 
 int main(int argc, char *argv[], char *env[])
 {
-    auto v1 = std::views::iota.operator()(1, 100);
-
-    auto is_multiple_3 = [](int i) -> bool { return i % 3 == 0; };
-
-    auto result = v1 | std::views::filter(is_multiple_3);
-
-    for (auto i : result)
     {
-        std::cout << i << std::endl;
+        auto v1            = std::views::iota.operator()(1, 100);
+        auto is_multiple_3 = [](int i) -> bool { return i % 3 == 0; };
+        auto result        = v1 | std::views::filter(is_multiple_3);
+        for (auto i : result)
+        {
+            std::cout << i << std::endl;
+        }
+    }
+    {
+        std::string str{"hello world i am ding"};
+        auto        result = std::views::all(str) | std::views::split(' ') |
+                      std::views::transform([](auto word) {
+                          return std::string_view(word.begin(), word.end());
+                      });
+        for (auto s : result)
+        {
+            std::cout << s << std::endl;
+        }
     }
 
     return 0;
